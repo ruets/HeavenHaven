752f1fc42c4a707dab4bb9612afddb7e2c02585b
Piryth <Arnaud.Endignous@etu.univ-grenoble-alpes.fr>
2022-12-20 10:09:02 +0100

    Client and Agent entities added.

---
+++ backend/model/agent.js
@@ -0,0 +1,7 @@
+import (User)
+class Agent extends User {
+
+    constructor(userId, name, secondName, phoneNb, email, password) {
+        super(userId, name, secondName, phoneNb, email, password);
+    }
+}
---
+++ backend/model/client.js
@@ -0,0 +1,39 @@
+import (User)
+class Client extends User {
+
+    sponsorCode
+    sponsorCodeUses
+
+
+    constructor(userId, name, secondName, phoneNb, email, password, sponsorCode, sponsorCodeUses) {
+        super(userId, name, secondName, phoneNb, email, password);
+        this._sponsorCode = sponsorCode;
+        this._sponsorCodeUses = sponsorCodeUses;
+    }
+
+    generateSponsorCode() {
+        //TODO : create sponsor code generation
+    }
+
+    /////////////////////////////////////////////////////////
+    // Getters et Setters
+    /////////////////////////////////////////////////////////
+    //TODO : create constraints in the setter and add it to the constructor
+
+
+    get sponsorCode() {
+        return this._sponsorCode;
+    }
+
+    set sponsorCode(value) {
+        this._sponsorCode = value;
+    }
+
+    get sponsorCodeUses() {
+        return this._sponsorCodeUses;
+    }
+
+    set sponsorCodeUses(value) {
+        this._sponsorCodeUses = value;
+    }
+}
