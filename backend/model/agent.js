bd9136113b2dd895bff0c4f9598f0ab417af6944
Piryth <Arnaud.Endignous@etu.univ-grenoble-alpes.fr>
2022-12-29 14:26:41 +0100

    Testing on Agent entity (same as User)

--- backend/model/agent.js
+++ backend/model/agent.js
@@ -1,7 +1,12 @@
+import User from "./user";
+
 import (User)
+
 class Agent extends User {
 
     constructor(userId, name, secondName, phoneNb, email, password) {
         super(userId, name, secondName, phoneNb, email, password);
     }
 }
+
+module.exports = Agent
