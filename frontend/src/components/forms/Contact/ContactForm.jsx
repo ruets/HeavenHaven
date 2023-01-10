f5e191d9bc26619bb2825302ee5ebeb2ec836ed1
merelp <paul.merel@etu.univ-grenoble-alpes.fr>
2023-01-10 11:32:32 +0100

    Contact Page and Contact Form Update

--- frontend/src/components/forms/Contact/ContactForm.jsx
+++ frontend/src/components/forms/Contact/ContactForm.jsx
@@ -1,9 +1,20 @@
 import "./ContactForm.scss";
+import Input from "../../fields/Input/Input";
 
 export function ContactForm() {
     return (
         <form className="contact">
             <h1> Contact Us</h1>
+            <p>Get in touch and let us know how we can help</p>
+            <div className="fields">
+                <div className="inputs-1"></div>
+                <Input type="email"
+                        name="email"
+                        label="Email">
+                </Input>
+                <Input>
+                </Input>
+            </div>
         </form>
 );
 }
