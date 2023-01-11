c1f4c5ccd7ae9d708cb4126baca2b05db0437015
merelp <paul.merel@etu.univ-grenoble-alpes.fr>
2023-01-11 08:22:00 +0100

    Contact Form Updated

--- frontend/src/components/forms/Contact/ContactForm.jsx
+++ frontend/src/components/forms/Contact/ContactForm.jsx
@@ -1,17 +1,42 @@
+import React, { useState, useCallback } from "react";
 import "./ContactForm.scss";
 import Input from "../../fields/Input/Input";
 import ArrowIcon from "../../../assets/img/right-arrow.svg";
 
 export function ContactForm() {
-    return (
-        <form className="contact">
+
+        const [name, setName] = useState("");
+
+        const onSubmitForm = useCallback(
+                (e) => {
+                    e.preventDefault();
+                    validateEmailFieldValue();
+                    if (errorMessageEmail === "") {
+                        console.log("Trying to connect");
+                        postData();
+                    }
+                },
+                [
+                    validateEmailFieldValue,
+                    errorMessageEmail,
+                    errorMessagePassword,
+                    setErrorMessagePassword,
+                    password,
+                ]
+        );
+
+
+        return (
+        <form className="contact" onSubmit={onSubmitForm}>
             <h1> Contact Us</h1>
             <p>Get in touch and let us know how we can help</p>
             <div className="fields">
                 <div className="inputs-1">
                 <Input type="text"
                         name="name"
-                        label="Name">
+                        label="Name"
+                        value={name}
+                        setInput={setName}>
                 </Input>
                 <Input type="email"
                         name="email"
