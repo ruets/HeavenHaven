b848619061ab5afbe82f05a560530ee58acfe138
STR <sruet.dev@gmail.com>
2023-01-13 14:14:23 +0100

    coment config file

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,6 @@
-// Fichier de configuration de l'application (port, nom de la base de données, etc.)
+// Fichier de configuration de l'application (port, clé de cryptage des tokens, etc.)
 // config du port
 exports.port = process.env.PORT || 3000;
 
+// clé de cryptage des tokens
 exports.secretKey = 'YgndskNRSrKFXnSmcKtmnoFXMxshyzcJGsixyDDQ';
