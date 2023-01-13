ffd4541298a90a049183a00734c0deb3162430bf
STR <sruet.dev@gmail.com>
2023-01-13 14:27:14 +0100

    adding routes for selling islands

--- backend/routes/islands.js
+++ backend/routes/islands.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const multer = require("../middleware/multer-configs/islands");
+
 const islandsCtrl = require("../controllers/islands");
 const auth = require("../middleware/auth");
 
 router.get('/', islandsCtrl.getAll);
 router.get('/:id', islandsCtrl.getOne);
 router.get("/trends", islandsCtrl.getTrends);
+router.post("/", auth, multer, islandsCtrl.sell);
 
 module.exports = router;
