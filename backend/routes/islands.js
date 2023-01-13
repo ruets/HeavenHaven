f0aa0970544fe0c6e245387782bd5068c7752957
STR <sruet.dev@gmail.com>
2023-01-13 16:37:41 +0100

    fix trends route

--- backend/routes/islands.js
+++ backend/routes/islands.js
@@ -6,17 +6,9 @@ const multer = require("../middleware/multer-configs/islands");
 const islandsCtrl = require("../controllers/islands");
 const auth = require("../middleware/auth");
 
-router.get('/', islandsCtrl.getAll);
-router.get('/:id', islandsCtrl.getOne);
 router.get("/trends", islandsCtrl.getTrends);
-router.post("/", auth, islandsCtrl.sell);
-
-const test = (req , res, next) => {
-    console.log(req.files);
-    
-    res.status(200).json({ message: "OK" });
-}
-
-router.post("/test", multer, test);
+router.get('/:id', islandsCtrl.getOne);
+router.get('/', islandsCtrl.getAll);
+// router.post("/", auth, islandsCtrl.sell);
 
 module.exports = router;
