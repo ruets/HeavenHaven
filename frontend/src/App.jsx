d74e64c1a8c181b071a2acb7df750d5ed867b027
Samuel Billot <samuel.billot@outlook.fr>
2023-01-14 11:55:06 +0100

    SellingForm part 2 initialized, input with checkbox working well

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,6 @@
-import { IndexPage } from "./pages/Index/IndexPage";
-import {IslandsPage} from "./pages/Islands/IslandsPage";
-import { LoginPage } from "./pages/Connection/Login/LoginPage";
-import { SignupPage } from "./pages/Connection/Signup/SignupPage";
-import { ForgotPage } from "./pages/Forgot/ForgotPage";
-import { SellingPage } from "./pages/Selling/SellingPage/SellingPage"
-import { SellingForm } from "./components/forms/Selling/SellingForm"
-import { ContactPage } from "./pages/Contact/ContactPage";
-import { NotFoundPage } from "./pages/NotFound/NotFoundPage";
+import { SellingForm } from "./components/forms/Selling/SellingForm";
 import { Header } from "./components/base/Header/Header";
 import { Footer } from "./components/base/Footer/Footer";
-import { ProfilePage } from "./pages/Profile/ProfilePage";
 import { Routes, Route } from "react-router-dom";
 import "./assets/scss/style.scss";
 import "./App.scss";
@@ -19,16 +10,7 @@ function App() {
         <div className="App">
             <Header />
             <Routes>
-                <Route path="/" element={<IndexPage />} />
-                <Route path="login" element={<LoginPage />} />
-                <Route path="forgot" element={<ForgotPage />} />
-                <Route path="signup" element={<SignupPage />} />
-                <Route path="islands/*" element={<IslandsPage />} />
-                <Route path="sell" element={<SellingPage />} />
-                <Route path="sell/form" element={<SellingForm/>}/>
-                <Route path="contact" element={<ContactPage/>}/>
-                <Route path="profile" element={<ProfilePage/>}/>
-                <Route path="*" element={<NotFoundPage />} />
+                <Route path="sell/form" element={<SellingForm />} />
             </Routes>
             <Footer />
         </div>
--- frontend/src/components/forms/Selling/SellingForm.jsx
+++ frontend/src/components/forms/Selling/SellingForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useRef } from "react";
 import Input from "../../../components/fields/Input/Input";
 import "./SellingForm.scss";
 
@@ -13,19 +13,44 @@ export function SellingForm() {
     const [startDate, setStartDate] = useState("");
     const [endDate, setEndDate] = useState("");
 
+    const [weather, setWeather] = useState("");
+    const [oldWeatherText, setOldWeatherText] = useState("");
+    const refToWeatherInput = useRef(null);
+    const refToWeatherCheckbox = useRef(null);
+
     const validateDate = useCallback(() => {
         console.log("Have to validate date here");
     });
 
-    const handleNextPage = useCallback(() => {
-        setPagesCount(pagesCount + 1);
-        console.log(pagesCount);
-    });
+    const handleNextPage = useCallback(
+        (e) => {
+            e.preventDefault();
+            setPagesCount(pagesCount + 1);
+        },
+        [pagesCount, setPagesCount]
+    );
 
-    if (pagesCount === 0) {
+    const handleCheckboxChange = useCallback(() => {
+        if (refToWeatherCheckbox.current.checked) {
+            setOldWeatherText(weather);
+            setWeather("");
+            refToWeatherInput.current.disabled = true;
+        } else {
+            refToWeatherInput.current.disabled = false;
+            setWeather(oldWeatherText);
+        }
+    }, [
+        refToWeatherCheckbox,
+        oldWeatherText,
+        setOldWeatherText,
+        weather,
+        setWeather,
+    ]);
+
+    if (pagesCount !== 0) {
         return (
             <div className="selling form-1">
-                <form className="selling" onSubmit={handleNextPage}>
+                <form className="selling" onSubmit={(e) => handleNextPage(e)}>
                     <div className="form-steps-selling">
                         <div className="step step-1">
                             <span className="ball filled"></span>
@@ -106,10 +131,10 @@ export function SellingForm() {
                 </form>
             </div>
         );
-    } else if (pagesCount === 1) {
+    } else if (pagesCount !== 1) {
         return (
             <div className="selling form-2">
-                <form className="selling" onSubmit={handleNextPage}>
+                <form className="selling" onSubmit={(e) => handleNextPage(e)}>
                     <div className="form-steps-selling">
                         <div className="step step-1">
                             <span className="ball filled"></span>
@@ -125,68 +150,41 @@ export function SellingForm() {
                         </div>
                     </div>
                     <div className="fields">
-                        <Input
-                            type="text"
-                            name="island-name"
-                            label="Island Name"
-                            value={islandName}
-                            setInput={setIslandName}
-                            required
-                        ></Input>
-                        <div className="coordinates">
-                            <Input
-                                type="text"
-                                name="longitude"
-                                label="Longitude"
-                                value={logitude}
-                                setInput={setLongitude}
-                                required
-                            ></Input>
-                            <Input
-                                type="text"
-                                name="latitude"
-                                label="Latitude"
-                                value={latitude}
-                                setInput={setLatitude}
-                                required
-                            ></Input>
+                        <div className="weather">
+                            <div className="left">
+                                <h3>Weather</h3>
+                                <input
+                                    type="text"
+                                    name="weather"
+                                    value={weather}
+                                    onChange={(e) => setWeather(e.target.value)}
+                                    ref={refToWeatherInput}
+                                />
+                                <input
+                                    type="checkbox"
+                                    name="weatherCheckbox"
+                                    ref={refToWeatherCheckbox}
+                                    onClick={handleCheckboxChange}
+                                />
+                            </div>
+                            <div className="right"></div>
                         </div>
-                        <Input
-                            type="text"
-                            name="surface"
-                            label="Surface"
-                            value={surface}
-                            setInput={setSurface}
-                            required
-                        ></Input>
-                        <Input
-                            type="text"
-                            name="reserve-price"
-                            label="Reserve Price"
-                            value={reservePrice}
-                            setInput={setReservePrice}
-                            required
-                        ></Input>
-                        <Input
-                            type="date"
-                            name="start-date"
-                            label="Start Date"
-                            value={startDate}
-                            setInput={setStartDate}
-                            required
-                        ></Input>
-                        <Input
-                            type="date"
-                            name="end-date"
-                            label="End Date"
-                            value={endDate}
-                            setInput={setEndDate}
-                            required
-                        ></Input>
+                        <div className="wildlife"></div>
+                        <div className="activities"></div>
+                        <div className="location"></div>
+                        <div className="pictures"></div>
                     </div>
-                    <button className="cta" type="submit">
-                        Next
-                    </button>
+                    <div className="buttons">
+                        <button
+                            className="cta"
+                            onClick={() => setPagesCount(pagesCount - 1)}
+                        >
+                            Back
+                        </button>
+                        <button className="cta" type="submit">
+                            Next
+                        </button>
+                    </div>
                 </form>
             </div>
         );
