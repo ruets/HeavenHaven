a5b4ea0ea753082c395b283cb95cf71c72ee863b
Samuel Billot <samuel.billot@outlook.fr>
2023-01-15 21:23:33 +0100

    Two different headers based on if the user is logged in or not. And changing the default one to the connected one if the user logs in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,29 +10,44 @@ import { NotFoundPage } from "./pages/NotFound/NotFoundPage";
 import { Header } from "./components/base/Header/Header";
 import { Footer } from "./components/base/Footer/Footer";
 import { ProfilePage } from "./pages/Profile/ProfilePage";
-import{IslandPresentationPage} from "./pages/IslandPresentation/IslandPresentationPage"
+import { IslandPresentationPage } from "./pages/IslandPresentation/IslandPresentationPage";
 import { Routes, Route } from "react-router-dom";
+import { createContext, useState } from "react";
 import "./assets/scss/style.scss";
 import "./App.scss";
 
+export const LoginContext = createContext({
+    loggedIn: false,
+    setLoggedIn: (auth) => {},
+});
+
 function App() {
+    const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
     return (
         <div className="App">
-            <Header />
-            <Routes>
-                <Route path="/" element={<IndexPage />} />
-                <Route path="login" element={<LoginPage />} />
-                <Route path="forgot" element={<ForgotPage />} />
-                <Route path="signup" element={<SignupPage />} />
-                <Route path="islands/*" element={<IslandsPage />} />
-                <Route path="sell" element={<SellingPage />} />
-                <Route path="sell/form" element={<SellingForm />} />
-                <Route path="contact" element={<ContactPage />} />
-                <Route path="profile" element={<ProfilePage />} />
-                <Route path="*" element={<NotFoundPage />} />
-                <Route path="presentation" element={<IslandPresentationPage/>}/>
-            </Routes>
-            <Footer />
+            <LoginContext.Provider
+                value={{ isUserLoggedIn, setIsUserLoggedIn }}
+            >
+                <Header />
+                <Routes>
+                    <Route path="/" element={<IndexPage />} />
+                    <Route path="login" element={<LoginPage />} />
+                    <Route path="forgot" element={<ForgotPage />} />
+                    <Route path="signup" element={<SignupPage />} />
+                    <Route path="islands/*" element={<IslandsPage />} />
+                    <Route path="sell" element={<SellingPage />} />
+                    <Route path="sell/form" element={<SellingForm />} />
+                    <Route path="contact" element={<ContactPage />} />
+                    <Route path="profile" element={<ProfilePage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                    <Route
+                        path="presentation"
+                        element={<IslandPresentationPage />}
+                    />
+                </Routes>
+                <Footer />
+            </LoginContext.Provider>
         </div>
     );
 }
