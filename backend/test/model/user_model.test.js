61140053f066874aa7a436a97b000211e2cc9dec
Piryth <Arnaud.Endignous@etu.univ-grenoble-alpes.fr>
2023-01-17 09:54:50 +0100

    agent unit testing started

--- backend/test/model/user_model.test.js
+++ backend/test/model/user_model.test.js
@@ -6,7 +6,10 @@ const prisma = new PrismaClient();
 
 //TODO : the testing is weird : sometimes it works, sometimes not. We need to figure out why
 
-//Testing on user model integrity
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////                           USER - CUSTOMER CRUD                               //////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
  describe("createUser", function () {
     
     //We try to create a new customer
@@ -15,39 +18,37 @@ const prisma = new PrismaClient();
        assert.doesNotThrow( async (done) => {
         
             //Firstly, we need to find if the record already exists
-            let findUSer = await prisma.user.findUnique({
+            let findUser = await prisma.user.findUnique({
                 where: {
                     email: "email"
                 }
-            }).then(async result => {
-                //Then, if it does not exist
-                if(result === null) {
-                    //We create a customer and test if it is stored into the database
-                    //We do not care about constraints on this layer, it must be done either in the frontend, either in the controllers
-                    let user = await prisma.user.create({
-                        data: {
-                            email: "email",
-                            password: "password",
-                            firstName: "Francis",
-                            lastName: "Boyd",
-                            phone: "0303030303",
-                    
-                            address: "51 St of the Test",
-                            apt: "91",
-                            city: "TestCity",
-                            zip: "029302882838",
-                            country: "Republic of Testing",
-                            customer : {
-                                create: {
-                                    sponsorCode: "HHHHH",
-                                }
+            })
+            //Then, if it does not exist
+            if(findUser=== null) {
+                //We create a customer and test if it is stored into the database
+                //We do not care about constraints on this layer, it must be done either in the frontend, either in the controllers
+                const user = await prisma.user.create({
+                    data: {
+                        email: "email",
+                        password: "password",
+                        firstName: "Francis",
+                        lastName: "Boyd",
+                        phone: "0303030303",
+                
+                        address: "51 St of the Test",
+                        apt: "91",
+                        city: "TestCity",
+                        zip: "029302882838",
+                        country: "Republic of Testing",
+                        customer : {
+                            create: {
+                                sponsorCode: "HHHHH",
                             }
                         }
-                        
-                    }).then(result => {});
-                }
-            });
-            
+                    }
+                    
+                });
+            }
         });
 
     }); 
@@ -78,7 +79,6 @@ describe("updateOneUser", function() {
     });
 });
 
-
 describe("selectOneUser", function() {
 
     //When done, we check if we can find the user previously created
@@ -95,7 +95,6 @@ describe("selectOneUser", function() {
 
   });
 });
-
 
 describe("deleteOneUser", function() {
     it("Should delete the created customer", async function() {
@@ -135,4 +134,93 @@ describe("deleteOneUser", function() {
         assert.equal(user, null);
 
     });
-})
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////                           USER - AGENT CRUD                                  //////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+describe("CreateAgent", function() {
+
+    it("Should create a new agent user",async function() {
+
+        assert.doesNotThrow(async () => {
+            //An agent requires a customer to be created
+        //So we first create a customer
+
+        const customer = await prisma.user.findUnique({
+            where: {
+                email: "email2"
+            }
+        });
+
+        if(customer === null) {
+            const createCustomer = await prisma.customer.create({
+                data: {
+                    user: {
+                        email: "email2",
+                        password: "password",
+                        firstName: "Francis",
+                        lastName: "Boyd",
+                        phone: "0303030303",
+                
+                        address: "51 St of the Test",
+                        apt: "91",
+                        city: "TestCity",
+                        zip: "029302882838",
+                        country: "Republic of Testing"
+                    },
+                    
+                    sponsorCode: "WWWWW"
+                }  
+            });
+
+            //After the customer is created, we can create the agent
+            const agent = await prisma.agent.create({
+                data: {
+                    user: {
+                        create: {
+                           email: "agentemail",
+                        password: "password",
+                        firstName: "Xavier",
+                        lastName: "Chanson",
+                        phone: "0405060708"
+                        }
+                        
+                    },
+                    
+                    customer: {
+                        connect: {
+                            idUser: createCustomer.idUser
+                        }
+                    }
+                }
+            });
+            
+        }});
+    });
+});
+
+// describe("update one agent", function(){
+
+//     it("Should update the agent user", async function() {
+
+//         const updateAgent = await prisma.user.update({
+//             where: {
+//                 email: "agentemail"
+//             },
+//             data: {
+//                 firstName: "Romuald"
+//             }
+//         });
+
+//         const agentUser = await prisma.user.findUnique({
+//             where: {
+//                 email: "agentemail"
+//             }
+//         });
+
+//         assert.equal(agentUser.firstName, "Romuald");
+//     });
+
+// });
