21d1ab6a8a9e44ea887d2e3a0e60d11d428639eb
STR <sruet.dev@gmail.com>
2023-01-18 06:50:54 +0100

    delete billing routes (useless at the moment)

--- backend/routes/billing.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const billingCtrl = require('../controllers/billing');
-const auth = require('../middleware/auth');
-
-router.post('/payToBid', auth, billingCtrl.payToBid);
-router.post('/commission', auth, billingCtrl.commission);
-router.post('/caution', auth, billingCtrl.caution);
