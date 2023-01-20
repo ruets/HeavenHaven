27e58b43e72e4c24c8f6f4edc5fc50ddd29710b0
merelp <paul.merel@etu.univ-grenoble-alpes.fr>
2023-01-20 12:57:56 +0100

    fix some bugs

--- frontend/src/components/forms/Selling/SellingForm.jsx
+++ frontend/src/components/forms/Selling/SellingForm.jsx
@@ -18,40 +18,28 @@ export function SellingForm() {
         return <ForbiddenPage />;
     }
 
-   
+    // Global variable
     const [pagesCount, setPagesCount] = useState(0);
     const navigate = useNavigate();
 
-    
-    /**
- * This function is used to increment the page counter
- */
-const handleNextPage = useCallback(() => {
-    // Increment the page counter
-    setPagesCount(pagesCount + 1);
-}, [pagesCount, setPagesCount]);
+    // Global functions
+    const handleNextPage = useCallback(() => {
+        setPagesCount(pagesCount + 1);
+    }, [pagesCount, setPagesCount]);
 
     function validateImage(image) {
-        // Get the file size in kilobytes (1 kilobyte = 1024 bytes)
         const maxFileSizeKilo = 8192;
         const fileSizeBytes = image[0].size;
         const fileSizeKilo = Math.round(fileSizeBytes / 1024);
-
-        // If the file size is greater than the maximum file size allowed
         if (fileSizeKilo >= maxFileSizeKilo) {
-            // Display the error message
             refToErrorMessageContent.current.innerText =
                 "The " + image.name + " image must be less than 8Mo.";
         } else {
-            // Hide the error message
             refToErrorMessageContent.current.innerText = "";
         }
     }
 
     function validateFile(file) {
-        // this function will validate the file size of the file
-        // the file parameter is an array of files, so we need to get the first element
-        // of the array to get the file object
         const maxFileSizeKilo = 8192;
         const fileSizeBytes = file[0].size;
         const fileSizeKilo = Math.round(fileSizeBytes / 1024);
@@ -63,70 +51,51 @@ const handleNextPage = useCallback(() => {
         }
     }
 
-    // Validate the text area
-function validateTextarea(text, checkbox) {
-    // If the checkbox is checked, return true
-    if (checkbox.checked) {
-        return true;
-    // If the checkbox is not checked, and the text is less than 200 characters or more than 625 characters
-    } else if (text.value.length < 200 || text.value.length > 625) {
-        // Add a red border to the text area
-        text.style.border = "1px red solid";
-        // Return false
-        return false;
-    // If the checkbox is not checked, and the text is between 200 and 625 characters
-    } else {
-        // Return true
-        return true;
+    function validateTextarea(text, checkbox) {
+        if (checkbox.checked) {
+            return true;
+        } else if (text.value.length < 200 || text.value.length > 625) {
+            text.style.border = "1px red solid";
+            return false;
+        } else {
+            return true;
+        }
     }
-}
 
     // Error Messages
     const refToErrorMessageGeneral = useRef(null);
     const refToErrorMessageContent = useRef(null);
 
-    
+    // Step 1
 
-    const [islandName, setIslandName] = useState(""); //initializing the islandName variable to empty string
-    const [longitude, setLongitude] = useState(""); //initializing the longitude variable to empty string
-    const [latitude, setLatitude] = useState(""); //initializing the latitude variable to empty string
-    const [surface, setSurface] = useState(""); //initializing the surface variable to empty string
-    const [country, setCountry] = useState(""); //initializing the country variable to empty string
-    const [continent, setContinent] = useState(""); //initializing the continent variable to empty string
-    const [reservePrice, setReservePrice] = useState(""); //initializing the reservePrice variable to empty string
-    const [startDate, setStartDate] = useState(""); //initializing the startDate variable to empty string
-    const [endDate, setEndDate] = useState(""); //initializing the endDate variable to empty string
+    const [islandName, setIslandName] = useState("");
+    const [longitude, setLongitude] = useState("");
+    const [latitude, setLatitude] = useState("");
+    const [surface, setSurface] = useState("");
+    const [country, setCountry] = useState("");
+    const [continent, setContinent] = useState("");
+    const [reservePrice, setReservePrice] = useState("");
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
 
-    // this function validates the dates entered by the user
-// if the ending date is before the starting date, then the user will see an error message
-// this function is called when the user changes the dates
-function validateDate() {
-    // if the ending date is before the starting date...
-    if (endDate <= startDate) {
-        // display the error message
-        refToErrorMessageGeneral.current.innerText =
-            "The ending date must be after the starting date";
-    } else {
-        // else, hide the error message
-        refToErrorMessageGeneral.current.innerText = "";
+    function validateDate() {
+        if (endDate <= startDate) {
+            refToErrorMessageGeneral.current.innerText =
+                "The ending date must be after the starting date";
+        } else {
+            refToErrorMessageGeneral.current.innerText = "";
+        }
     }
-}
 
     function validateFirstStep(e) {
-        // Prevent the default behaviour of the form (i.e. to submit the form)
         e.preventDefault();
-        // Validate the date and time
         validateDate();
-        // Validate the error message
         validateErrorMessage(refToErrorMessageGeneral.current.innerText);
     }
 
     const validateErrorMessage = useCallback((errorMessageGeneral) => {
         if (errorMessageGeneral === "") {
-            // If the error message is empty, proceed to the next page
             handleNextPage();
-        } else {
-            // If the error message is not empty, stay on the current page
         }
     });
 
@@ -186,23 +155,16 @@ function validateDate() {
     const [picturesImage, setPicturesImage] = useState([]);
     const [picturesImageName, setPicturesImageName] = useState("");
 
-    
+    // Weather
     const handleWeatherCheckboxChange = useCallback(() => {
-    // Get the current value of the checkbox
-    const isChecked = refToWeatherCheckbox.current.checked;
-    // If the checkbox is checked, 
-    // then we need to store the current value of the weather input 
-    // and clear it. We also disable the input field.
-    if (isChecked) {
-        setOldWeatherText(weather);
-        setWeather("");
-        refToWeatherInput.current.disabled = true;
-    // If the checkbox is not checked, 
-    // then we need to enable the input field and restore the old value.
-    } else {
-        refToWeatherInput.current.disabled = false;
-        setWeather(oldWeatherText);
-    }
+        if (refToWeatherCheckbox.current.checked) {
+            setOldWeatherText(weather);
+            setWeather("");
+            refToWeatherInput.current.disabled = true;
+        } else {
+            refToWeatherInput.current.disabled = false;
+            setWeather(oldWeatherText);
+        }
     }, [
         refToWeatherCheckbox,
         oldWeatherText,
@@ -211,19 +173,14 @@ function validateDate() {
         setWeather,
     ]);
 
-    // Weather
     const handleWeatherImageClick = useCallback(() => {
-    // when the user clicks the weather image, the hidden file input is clicked
-    refToHiddenWeatherFileInput.current.click();
+        refToHiddenWeatherFileInput.current.click();
     }, [refToHiddenWeatherFileInput]);
 
     const handleWeatherImageChange = useCallback(() => {
-        // when the user selects a file, the file input's files are stored as an array
         const weatherImage = refToHiddenWeatherFileInput.current.files;
-        // the name of the file is stored in state
         setWeatherImageName(weatherImage[0].name);
 
-        // and the image is validated
         validateImage(weatherImage);
     }, [refToHiddenWeatherFileInput, setWeatherImageName]);
 
@@ -249,31 +206,21 @@ function validateDate() {
         refToHiddenWildlifeFileInput.current.click();
     }, [refToHiddenWildlifeFileInput]);
 
-    // use useCallback to assign a function to a variable that will only be recreated when necessary
     const handleWildlifeImageChange = useCallback(() => {
-        // assign the value of the selected file to a variable
         const wildlifeImage = refToHiddenWildlifeFileInput.current.files;
-        // assign the name of the selected file to a variable
         setWildlifeImageName(wildlifeImage[0].name);
 
-        // call the function validateImage to validate the selected file
         validateImage(wildlifeImage);
     }, [refToHiddenWildlifeFileInput, setWildlifeImageName]);
 
     // Activities
     const handleActivitiesCheckboxChange = useCallback(() => {
-        // Check if the checkbox is checked
         if (refToActivitiesCheckbox.current.checked) {
-            // If checked, store the current input value
             setOldActivitiesText(activities);
-            // Clear the input field
             setActivities("");
-            // Disable the input field
             refToActivitiesInput.current.disabled = true;
         } else {
-            // If not checked, enable the input field
             refToActivitiesInput.current.disabled = false;
-            // Set the input field value to the stored value
             setActivities(oldActivitiesText);
         }
     }, [
@@ -285,36 +232,24 @@ function validateDate() {
     ]);
 
     const handleActivitiesImageClick = useCallback(() => {
-        // 1. Get the 'current' property from the ref
-        // 2. Call the 'click' method on that property
         refToHiddenActivitiesFileInput.current.click();
     }, [refToHiddenActivitiesFileInput]);
 
-    // Create a callback function to handle the activities image change.
     const handleActivitiesImageChange = useCallback(() => {
-    // Get the activities image.
-    const activitiesImage = refToHiddenActivitiesFileInput.current.files;
+        const activitiesImage = refToHiddenActivitiesFileInput.current.files;
+        setActivitiesImageName(activitiesImage[0].name);
 
-    // Set the activities image name.
-    setActivitiesImageName(activitiesImage[0].name);
-
-    // Validate the activities image.
-    validateImage(activitiesImage);
-}, [refToHiddenActivitiesFileInput, setActivitiesImageName]);
+        validateImage(activitiesImage);
+    }, [refToHiddenActivitiesFileInput, setActivitiesImageName]);
 
     // Location
     const handleLocationCheckboxChange = useCallback(() => {
-        // When the checkbox is checked, store the location in the oldLocationText variable
         if (refToLocationCheckbox.current.checked) {
             setOldLocationText(location);
-            // Reset the location to an empty string
             setLocation("");
-            // Disable the location input
             refToLocationInput.current.disabled = true;
         } else {
-            // When the checkbox is unchecked, enable the location input
             refToLocationInput.current.disabled = false;
-            // Restore the location from the oldLocationText variable
             setLocation(oldLocationText);
         }
     }, [
@@ -326,55 +261,42 @@ function validateDate() {
     ]);
 
     const handleLocationImageClick = useCallback(() => {
-        // refToHiddenLocationFileInput is the reference to the hidden input element
-        // which we will use to trigger the file input when the user clicks on the image
-        // element
         refToHiddenLocationFileInput.current.click();
     }, [refToHiddenLocationFileInput]);
 
     const handleLocationImageChange = useCallback(() => {
-        // Get the file input's file list
         const locationImage = refToHiddenLocationFileInput.current.files;
-        // Get the file name from the file list
         setLocationImageName(locationImage[0].name);
 
-        // Validate the file
         validateImage(locationImage);
     }, [refToHiddenLocationFileInput, setLocationImageName]);
 
     // Pictures
 
     const handlePicturesImageClick = useCallback(() => {
-        // When the user clicks the image of the pictures, the hidden input element is clicked
         refToHiddenPicturesFileInput.current.click();
     }, [refToHiddenPicturesFileInput]);
 
     const handlePicturesImageChange = useCallback(() => {
-    // 1. Get the file input element
-    const picturesImage = refToHiddenPicturesFileInput.current.files;
+        const picturesImage = refToHiddenPicturesFileInput.current.files;
 
-    // 2. Check if the number of images is less than 5
-    if (picturesImage.length > 5) {
-        // 3. If the number of images is greater than 5, display the error message
-        refToErrorMessageContent.current.innerText =
-            "You cannot put more than 5 files in the Pictures field";
-        setPicturesImageName("");
-    } else {
-        // 4. If the number of images is less than 5, validate the images
-        const num = picturesImage.length - 1;
-
-        // 5. If the number of images is greater than 1, display the number of images
-        if (picturesImage.length > 1) {
-            setPicturesImageName(
-                picturesImage[0].name + " + " + num.toString()
-            );
+        if (picturesImage.length > 5) {
+            refToErrorMessageContent.current.innerText =
+                "You cannot put more than 5 files in the Pictures field";
+            setPicturesImageName("");
         } else {
-            // 6. If the number of images is less than 1, display the image name
-            setPicturesImageName(picturesImage[0].name);
+            const num = picturesImage.length - 1;
+
+            if (picturesImage.length > 1) {
+                setPicturesImageName(
+                    picturesImage[0].name + " + " + num.toString()
+                );
+            } else {
+                setPicturesImageName(picturesImage[0].name);
+            }
+            validateImage(picturesImage);
         }
-        validateImage(picturesImage);
-    }
-}, [refToHiddenPicturesFileInput, setPicturesImageName]);
+    }, [refToHiddenPicturesFileInput, setPicturesImageName]);
 
     const validateSecondStep = useCallback(
         (e) => {
@@ -453,45 +375,27 @@ function validateDate() {
 
     // Step 3
 
-    // This is the reference to the hidden file input element
     const refToHiddenOwnershipFileInput = useRef(null);
-
-    // This is the reference to the label that is clickable and shows the filename
     const refToLabelForOwnershipFileInput = useRef(null);
-
-    // This is the array that holds the file object
     const [ownershipDocument, setOwnershipDocument] = useState([]);
-
-    // This is the file name
     const [ownershipDocumentName, setOwnershipDocumentName] = useState("");
 
     const refToErrorMessageDocument = useRef(null);
 
     const handleOwnershipDocumentClick = useCallback(() => {
-        // This function is called when the user clicks on the "Upload" button.
-        // It calls the click() method on the hidden input element.
         refToHiddenOwnershipFileInput.current.click();
     }, [refToHiddenOwnershipFileInput]);
 
-    // This callback is triggered when the user selects a new file from the file picker
-const handleOwnershipDocumentChange = useCallback(() => {
-    // Get the file that was selected by the user
-    const ownershipDocument = refToHiddenOwnershipFileInput.current.files;
+    const handleOwnershipDocumentChange = useCallback(() => {
+        const ownershipDocument = refToHiddenOwnershipFileInput.current.files;
+        setOwnershipDocumentName(ownershipDocument[0].name);
 
-    // Store the file's name so that it can be displayed
-    setOwnershipDocumentName(ownershipDocument[0].name);
-
-    // Validate the selected file
-    validateFile(ownershipDocument);
-}, [refToHiddenOwnershipFileInput, setOwnershipDocumentName]);
+        validateFile(ownershipDocument);
+    }, [refToHiddenOwnershipFileInput, setOwnershipDocumentName]);
 
     const postData = async () => {
         var formData = new FormData();
 
-            // Create a new FormData object
-        var formData = new FormData();
-    
-        // Add the name, area, latitude, longitude, country, continent, weather, wildlife, activities, location, price, startDate and endDate to the formData object
         formData.append("name", islandName);
         formData.append("area", surface);
         formData.append("latitude", latitude);
@@ -536,13 +440,13 @@ const handleOwnershipDocumentChange = useCallback(() => {
     }
 
     const validateThirdStep = useCallback((e) => {
-        e.preventDefault(); // prevents the default action from occurring
-        if (ownershipDocumentName === "") { // checks if the ownershipDocumentName is empty
+        e.preventDefault();
+        if (ownershipDocumentName === "") {
             refToErrorMessageDocument.current.innerText =
-                "Please put your ownership in the requested field."; // if empty, display an error message
+                "Please put your ownership in the requested field.";
         } else {
-            postData(); // if not, send the data to the server
-            handleNextPage(pagesCount + 1); // and then move to the next page
+            postData();
+            handleNextPage(pagesCount + 1);
         }
     });
 
